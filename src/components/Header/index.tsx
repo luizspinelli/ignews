442642fae1159fc5267702005f8d811372dc3e03
Luiz Spinelli <luiz.luchi@gmail.com>
2021-05-17 20:46:04 -0300

    refactoring header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
+import { ActiveLink } from '../ActiveLink';
 import { SignInButton } from '../SignInButton';
-import Link from 'next/link'
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -8,12 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active} >Home</a>
-          </Link>
-          <Link href="/posts">
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
